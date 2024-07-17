6699aef57d242b490cafec3aa2fedbda984692a2
Tugs <rolandtugaoen013@gmail.com>
2024-07-18 03:21:12 +0800

    Add Class

--- routes/web.php
+++ routes/web.php
@@ -3,35 +3,18 @@
 use Illuminate\Cache\Events\RetrievingKey;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-$jobs = [
-    [
-        'id' => '1',
-        'title' => 'Dancer',
-        'salary' => 'P50,000'
-    ],
-    [
-        'id' => '2',
-        'title' => 'Singer',
-        'salary' => 'P50,000'
-    ],
-    [
-        'id' => '3',
-        'title' => 'Butcher',
-        'salary' => 'P50,000'
-    ],
-];
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs){
-    return view('jobs', ['jobs' => $jobs]);
+Route::get('/jobs', function () {
+    return view('jobs', ['jobs' => Job::all()]);
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs){
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
     return view('job', ['job' => $job]);
 });
 
