492a30725f07ce00ca896f7bd59be493a8762c44
Tugs <rolandtugaoen013@gmail.com>
2024-07-23 03:14:55 +0800

    Add Factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,37 +1,13 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job {
-    public static function all(): array {
-        return [
-            [
-                'id' => '1',
-                'title' => '    ',
-                'salary' => 'P50,000'
-            ],
-            [
-                'id' => '2',
-                'title' => 'Singer',
-                'salary' => 'P50,000'
-            ],
-            [
-                'id' => '3',
-                'title' => 'Butcher',
-                'salary' => 'P50,000'
-            ],
-        ];
-    }
+class Job extends Model{
+    use HasFactory;
 
-    public static function find(int $id): array {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+    protected $table = 'job_listing';
 
-        if (!$job)
-        {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
